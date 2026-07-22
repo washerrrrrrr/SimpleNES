aeca728f76a65d22e5fa280bfaebb0c6fec5f312
eyhtehdtrju5eyt <jakewasher5@gmail.com>
2026-07-22 23:31:19 +0100

    emu header update

--- include/Emulator.h
+++ include/Emulator.h
@@ -3,6 +3,9 @@
 #include <SFML/Graphics.hpp>
 #include <chrono>
 #include <filesystem>
+#include <vector>
+#include <iostream>
+#include <string>
 #include "APU/APU.h"
 #include "AudioPlayer.h"
 #include "CPU.h"
@@ -10,11 +13,12 @@
 #include "MainBus.h"
 #include "PPU.h"
 #include "PictureBus.h"
-
+#include <string>
 
 extern bool noUnfocusPause;
 extern int definedFps;
 extern bool emuStats;
+extern bool memEdit;
 
 struct emulatorStatsUI {
     sf::Clock clock;
@@ -25,8 +29,34 @@ struct emulatorStatsUI {
     sf::Text statsText;
 };
 
+
 namespace sn
 {
+
+struct loadsave_State{
+    std::string emu_Memory; //Memory from the emulator 
+    std::string emu_registers; //Register info from the emulator
+    std::filesystem::path saveDestination; //where the savestate file is saved.
+
+    //CPU getting
+    Address PC;
+    Byte SP;
+    Byte A_R;
+    Byte X_R;
+    Byte Y_R;
+
+    //these are booleans.
+    bool F_C;
+    bool F_Z;
+    bool F_I;
+    bool F_D;
+    bool F_V;
+    bool F_N;
+
+    std::vector<unsigned char> v_mem;
+    //unsigned int temp;
+};
+
 using TimePoint          = std::chrono::high_resolution_clock::time_point;
 using Duration           = std::chrono::high_resolution_clock::duration;
 
@@ -37,13 +67,15 @@ class Emulator
 {
 public:
     Emulator();
-    void run(std::string rom_path);
+    void run(std::string rom_path, Emulator* emuAddress);
     void setVideoWidth(int width);
     void setVideoHeight(int height);
     void setVideoScale(float scale);
     void setKeys(std::vector<sf::Keyboard::Key>& p1, std::vector<sf::Keyboard::Key>& p2);
     void muteAudio();
     void savestate();
+    void loadstate(bool isGame);
+    CPU* passCPU();
 
 
 private:
@@ -51,6 +83,7 @@ private:
     Byte                    DMCDMA(Address addr);
 
     CPU                     m_cpu;
+    //int                     cpuAddress() { return &m_cpu }
 
     AudioPlayer             m_audioPlayer;
 
